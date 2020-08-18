6ed1d404c9fcafe2589d38622ca0e59f65b3bcf5
Mouaad Aallam <mouaad@aallam.com>
2020-08-19 00:31:15 +0200

    apply explicit api

--- PreferencesFlow/src/main/java/com/aallam/preferencesflow/Preference.kt
+++ PreferencesFlow/src/main/java/com/aallam/preferencesflow/Preference.kt
@@ -12,57 +12,41 @@ public interface Preference<T> {
     /**
      * The key for which this preference will store and retrieve values.
      **/
-    val key: String
+    public val key: String
 
     /**
      * The value used if none is stored.
      **/
-    val defaultValue: T
+    public val defaultValue: T
 
     /**
      * Retrieve the current value for this preference.
      */
-    fun get(): T?
+    public fun get(): T?
 
     /**
      * Change this preference's stored value to [value].
      */
-    fun set(value: T)
+    public fun set(value: T)
 
     /**
      * Returns true if this preference has a stored value.
      **/
-    fun isSet(): Boolean
+    public fun isSet(): Boolean
 
     /**
      * Delete the stored value for this preference, if any.
      **/
-    fun delete()
+    public fun delete()
 
     /**
      * Observe changes to this preference.
      */
     @OptIn(ExperimentalCoroutinesApi::class)
-    fun asFlow(): Flow<T>
+    public fun asFlow(): Flow<T>
 
     /**
      * An action which stores a new value for this preference.
      */
-    fun asCollector(): FlowCollector<T>
-
-    /**
-     * Converts instances of [T] to be stored and retrieved as Strings SharedPreferences.
-     */
-    interface Converter<T> {
-
-        /**
-         * Deserialize to an instance of [T].
-         */
-        fun deserialize(serialized: String): T
-
-        /**
-         * Serialize the [value] to a String.
-         **/
-        fun serialize(value: T): String
-    }
+    public fun asCollector(): FlowCollector<T>
 }
--- PreferencesFlow/src/main/java/com/aallam/preferencesflow/PreferencesFlow.kt
+++ PreferencesFlow/src/main/java/com/aallam/preferencesflow/PreferencesFlow.kt
@@ -2,82 +2,80 @@
 
 package com.aallam.preferencesflow
 
-import android.content.Context
 import android.content.SharedPreferences
 import com.aallam.preferencesflow.internal.AndroidPreferencesFlow
 import kotlinx.serialization.KSerializer
-import androidx.preference.Preference as AndroidxPreference
 
-interface PreferencesFlow {
+public interface PreferencesFlow {
     /**
      * Create a boolean preference for [key]. Default is false.
      */
-    fun getBoolean(key: String): Preference<Boolean>
+    public fun getBoolean(key: String): Preference<Boolean>
 
     /**
      * Create a boolean preference for [key] with a default of [defaultValue].
      */
-    fun getBoolean(key: String, defaultValue: Boolean): Preference<Boolean>
+    public fun getBoolean(key: String, defaultValue: Boolean): Preference<Boolean>
 
     /**
      * Create a float preference for [key]. Default is 0.
      */
-    fun getFloat(key: String): Preference<Float>
+    public fun getFloat(key: String): Preference<Float>
 
     /**
      * Create a [Float] preference for [key] with a default of [defaultValue].
      */
-    fun getFloat(key: String, defaultValue: Float): Preference<Float>
+    public fun getFloat(key: String, defaultValue: Float): Preference<Float>
 
     /**
      * Create a float preference for [key]. Default is 0.
      */
-    fun getInteger(key: String): Preference<Int>
+    public fun getInteger(key: String): Preference<Int>
 
     /**
      * Create a [Float] preference for [key] with a default of [defaultValue].
      */
-    fun getInteger(key: String, defaultValue: Int): Preference<Int>
+    public fun getInteger(key: String, defaultValue: Int): Preference<Int>
 
     /**
      * Create a float preference for [key]. Default is 0L.
      */
-    fun getLong(key: String): Preference<Long>
+    public fun getLong(key: String): Preference<Long>
 
     /**
      * Create a [Long] preference for [key] with a default of [defaultValue].
      */
-    fun getLong(key: String, defaultValue: Long): Preference<Long>
+    public fun getLong(key: String, defaultValue: Long): Preference<Long>
 
     /**
      * Create a preference for type [T] for [key] with a default of [defaultValue].
      */
-    fun <T> getSerializable(key: String, defaultValue: T, serializable: KSerializer<T>): Preference<T>
+    public fun <T> getSerializable(key: String, defaultValue: T, serializable: KSerializer<T>): Preference<T>
 
     /**
      * Create a float preference for [key]. Default is empty string.
      */
-    fun getString(key: String): Preference<String?>
+    public fun getString(key: String): Preference<String?>
 
     /**
      * Create a [String] preference for [key] with a default of [defaultValue].
      */
-    fun getString(key: String, defaultValue: String): Preference<String?>
+    public fun getString(key: String, defaultValue: String): Preference<String?>
 
     /**
      * Create a string set preference for {@code key}. Default is [EmptySet].
      */
-    fun getStringSet(key: String): Preference<Set<String>?>
+    public fun getStringSet(key: String): Preference<Set<String>?>
 
     /**
      * Create a [String] set preference for [key] with a default of [defaultValue].
      */
-    fun getStringSet(key: String, defaultValue: Set<String>?): Preference<Set<String>?>
+    public fun getStringSet(key: String, defaultValue: Set<String>?): Preference<Set<String>?>
 
     /**
      * Remove all values from the preferences
      */
-    fun clear()
+    public fun clear()
 }
 
 /**
